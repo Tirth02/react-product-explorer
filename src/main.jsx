71604f4c619029956751ca4f8139dec6d13d5289
Tirth02 <pateltirth0902@gmail.com>
2026-01-07 22:56:31 -0500

    Setup routing for project using react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,44 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ProductsPage from './pages/ProductsPage.jsx'
+import ProductPage from './pages/ProductPage.jsx'
+import RandomProductPage from './pages/RandomProductPage.jsx'
+import NotFound from './pages/NotFound.jsx'
+import Home from './pages/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/products',
+        element: <ProductsPage/>
+      },
+      {
+        path: '/products/:id',
+        element: <ProductPage/>
+      },
+      {
+        path: '/random',
+        element: <RandomProductPage/>
+      },
+      {
+        path: "*",
+        element: <NotFound/>
+      }
+    ],
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
